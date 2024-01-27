56f3fcdaab7eb22e6b7e02fb249a2f0787050a44
duynb194261 <duy.nb194261@sis.hust.edu.vn>
2024-01-27 12:42:20 +0700

    first

---
+++ src/main/java/duy/nb/finalproject/demo/repository/SavedPostRepository.java
@@ -0,0 +1,14 @@
+package duy.nb.finalproject.demo.repository;
+
+import duy.nb.finalproject.demo.entities.SavedPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SavedPostRepository extends JpaRepository<SavedPost, Integer> {
+    List<SavedPost> findByUserId(int id);
+
+    List<SavedPost> findByUserIdAndPostId(int userId, int postId );
+
+    List<SavedPost> findByPostId(int postId);
+}
